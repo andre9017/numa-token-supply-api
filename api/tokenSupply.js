d92a0d5ef96b1bd4ac48d5fa03c432a0bca21f09
andre9017 <68817389+andre9017@users.noreply.github.com>
2024-12-18 23:07:41 +0000

    Update endpoint

--- api/tokenSupply.js
+++ api/tokenSupply.js
@@ -1,4 +1,8 @@
+const express = require("express");
 const { ethers } = require("ethers");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Connect to Arbitrum
 const provider = new ethers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
@@ -7,37 +11,39 @@ const provider = new ethers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
 const contractAddress = "0x7fb7ede54259cb3d4e1eaf230c7e2b1ffc951e9a"; // NUMA Contract Address
 const abi = [
   {
-    "constant": true,
-    "inputs": [],
-    "name": "tokenSupply",
-    "outputs": [{ "name": "", "type": "uint256" }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
 ];
 
-// Contract Instance
+// Instance of Contract
 const contract = new ethers.Contract(contractAddress, abi, provider);
 
-// Export the handler for Vercel
-module.exports = async (req, res) => {
+// Endpoint for total supply
+app.get("/tokenSupply", async (req, res) => {
   try {
-    // Fetch token Supply
-    const tokenSupply = await contract.tokenSupply();
-    const formattedSupply = ethers.formatUnits(tokenSupply, 18); // Number to 18 d.p.
+    const totalSupply = await contract.totalSupply();
+    const formattedSupply = ethers.formatUnits(totalSupply, 18); // Number to 18 d.p.
 
-    // Return JSON response
     res.status(200).json({
       data: {
-        tokenSupply: formattedSupply
-      }
+        total_supply: formattedSupply,
+      },
     });
   } catch (error) {
-    // Handle errors
     res.status(500).json({
       success: false,
-      error: error.message
+      error: error.message,
     });
   }
-};
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
